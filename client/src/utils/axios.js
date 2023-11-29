ddf35e118f4351bdf327f240acca8b47f1db3288
ptnminh <minhphan113@gmail.com>
2023-11-29 23:30:16 +0700

    update: change backend endpoint for deploy

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,41 +1,41 @@
 import axios from "axios";
 const getToken = () => JSON.parse(localStorage.getItem("token"));
 
-export const baseURL = "http://localhost:3333/";
+export const baseURL = "https://final.clinus.live";
 export const axiosInstance = axios.create({
-    baseURL: baseURL,
-    timeout: 10000,
-    headers: {
-        Authorization: `Bearer ${getToken()}`,
-        post: {
-            "Content-Type": "application/json",
-        },
-        put: {
-            "Content-Type": "application/json",
-        },
+  baseURL: baseURL,
+  timeout: 10000,
+  headers: {
+    Authorization: `Bearer ${getToken()}`,
+    post: {
+      "Content-Type": "application/json",
     },
+    put: {
+      "Content-Type": "application/json",
+    },
+  },
 });
 
 axios.interceptors.request.use(
-    (request) => {
-        console.log(request);
-        // Edit request config
-        return request;
-    },
-    (error) => {
-        console.log(error);
-        return Promise.reject(error);
-    }
+  (request) => {
+    console.log(request);
+    // Edit request config
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
 );
 
 axios.interceptors.response.use(
-    (response) => {
-        console.log(response);
-        // Edit response config
-        return response;
-    },
-    (error) => {
-        console.log(error);
-        return Promise.reject(error);
-    }
+  (response) => {
+    console.log(response);
+    // Edit response config
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
 );
