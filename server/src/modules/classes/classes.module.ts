91c28c7ac17f68d6571a83fe6cf07ff647dc1530
ptnminh <minhphan113@gmail.com>
2023-12-10 00:26:20 +0700

    fix: lack of imported service

--- server/src/modules/classes/classes.module.ts
+++ server/src/modules/classes/classes.module.ts
@@ -3,9 +3,10 @@ import { ClassesService } from './classes.service';
 import { ClassesController } from './classes.controller';
 import { PrismaService } from 'src/prisma.service';
 import { AuthService } from '../auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [ClassesController],
-  providers: [ClassesService, PrismaService, AuthService],
+  providers: [ClassesService, PrismaService, AuthService, JwtService],
 })
 export class ClassesModule {}
