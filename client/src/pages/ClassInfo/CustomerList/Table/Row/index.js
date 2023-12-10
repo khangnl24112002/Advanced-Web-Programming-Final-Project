3b85dde87fe6ee14e775afcf134a48efeacc42a6
Michael Thinh <michaelthinh1008@gmail.com>
2023-12-10 17:28:16 +0700

    Add: ClassInfo fetch APIs

--- client/src/pages/ClassInfo/CustomerList/Table/Row/index.js
+++ client/src/pages/ClassInfo/CustomerList/Table/Row/index.js
@@ -12,10 +12,12 @@ const Row = ({
     setActiveTable,
     activeId,
     setActiveId,
+    onChoose,
 }) => {
     const handleClick = (id) => {
         setActiveTable(true);
         setActiveId(id);
+        onChoose(id);
     };
 
     return (
@@ -37,15 +39,13 @@ const Row = ({
                 <div className={styles.col}>
                     <div
                         className={styles.item}
-                        onClick={() => handleClick(item.id)}
+                        onClick={() => handleClick(item)}
                     >
                         <div className={styles.avatar}>
-                            <img src={item.avatar} alt="Avatar" />
+                            <img src="/assets/sample-avatar.jpg" alt="Avatar" />
                         </div>
                         <div className={styles.details}>
-                            <div className={styles.user}>{item.user}</div>
-                            <div className={styles.login}>{item.login}</div>
-                            <div className={styles.email}>{item.email}</div>
+                            <div className={styles.user}>{item.name}</div>
                         </div>
                     </div>
                 </div>
@@ -64,7 +64,7 @@ const Row = ({
                 </div>
                 <div className={styles.col}>
                     <div className={cn("status-green-dark", styles.purchase)}>
-                        {item.purchase}
+                        {item.role}
                     </div>
                 </div>
                 {/* <div className={styles.col}>{item.comments}</div>
--- client/src/pages/ClassInfo/CustomerList/index.js
+++ client/src/pages/ClassInfo/CustomerList/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./CustomerList.module.sass";
 import cn from "classnames";
 import Card from "../../../components/Card";
@@ -13,174 +13,238 @@ import Modal from "./Modal";
 import Icon from "../../../components/Icon";
 import { errorToast, successToast } from "../../../utils/toast";
 import { useAuth } from "../../../hooks/useAuth";
+import { userServices } from "../../../services/UserServices";
+import { useParams } from "react-router-dom";
+import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
 // const navigation = ["Active", "New"];
 
 const CustomerList = () => {
-  // Lấy userInfo
-  const { user } = useAuth();
+    // Lấy userInfo
+    const { user } = useAuth();
 
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [search, setSearch] = useState("");
-  const [visible, setVisible] = useState(false);
+    const { token } = useAuth();
+    const [teachers, setTeachers] = useState([]);
 
-  const handleSubmit = (e) => {
-    alert();
-  };
-  const handleInviteMember = () => {
-    return successToast("Đã gửi lời mời!", 2000);
-  };
-  const [openModal, setOpenModal] = useState(false);
-  const [email, setEmail] = useState(true);
-  const [content, setContent] = useState(null);
-  const [urlClass, setUrlClass] = useState("http://example.com");
-  // Xử lí việc rời khỏi lớp học
-  const handleOutClass = () => {};
+    const [students, setStudents] = useState([]);
 
-  // Modal xử lí việc lấy url lớp học
-  const handleGetUrl = () => {
-    setOpenModal(true);
-    setContent(
-      <>
-        <div className={cn("title-green", styles.modaltitle)}>
-          Lấy URL lớp học
-        </div>
-        <div className={styles.info}>
-          Đây là đường dẫn đến lớp học của bạn <a href={urlClass}>{urlClass}</a>
-        </div>
-        <div className={styles.foot}>
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            className={cn("button-stroke", styles.button)}
-          >
-            <span>Quay lại</span>
-          </button>
-          <button
-            onClick={() => {
-              navigator.clipboard
-                .writeText(urlClass)
-                .then(() => {
-                  return successToast("Đã sao chép!", 1000);
-                })
-                .catch((err) => {
-                  return errorToast("Sao chép thất bại!", 1000);
+    const [activeUser, setActiveUser] = useState({});
+
+    const handleActive = (user) => {
+        setActiveUser(user);
+        console.log(user);
+    };
+
+    const [isLoading, setIsLoading] = useState(false);
+    const { classId } = useParams();
+    console.log(classId);
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const response = await userServices.getClassDetail(token, classId);
+
+            const responseData = await response.data;
+
+            const loadTeachers = [];
+            for (const key in responseData.teachers) {
+                loadTeachers.push({
+                    key: key,
+                    name:
+                        responseData.teachers[key].firstName +
+                        " " +
+                        responseData.teachers[key].lastName,
+                    email: responseData.teachers[key].email,
+                    role: "Giáo viên",
                 });
-            }}
-            className={cn("button", styles.button)}
-          >
-            <span>Copy</span>
-            <Icon name="arrow-right" size="24" />
-          </button>
-        </div>
-      </>
-    );
-  };
+            }
+            console.log(loadTeachers);
+            setTeachers(loadTeachers);
 
-  // Modal để thêm học sinh
-  const handleAddingStudent = () => {
-    setOpenModal(true);
-    setContent(
-      <>
-        <div className={cn("title-green", styles.modaltitle)}>
-          Thêm thành viên
-        </div>
-        <div className={styles.info}>
-          Thêm các thành viên vào lớp học của bạn.
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "flex-end",
-            gap: "10px",
-          }}
-        >
-          <TextInput
-            className={styles.field}
-            label="Địa chỉ email"
-            name="title"
-            type="text"
-            required
-          />
-          <button
-            className={cn("button-stroke", styles.button)}
-            onClick={handleInviteMember}
-          >
-            <span>Thêm</span>
-          </button>
-        </div>
-        <div className={styles.foot}>
-          <button
-            className={cn("button-stroke", styles.button)}
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            <span>Quay lại</span>
-          </button>
-        </div>
-      </>
-    );
-  };
-  // Modal để show xác nhận rời khỏi lớp
-  const handleOutGroup = () => {
-    setOpenModal(true);
-    setContent(
-      <>
-        <div className={cn("title-green", styles.modaltitle)}>
-          Xác nhận rời lớp
-        </div>
-        <div className={styles.info}>
-          Bạn thật sự muốn rời khỏi lớp học này chứ?
-        </div>
-        <div className={styles.foot}>
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            className={cn("button-stroke", styles.button)}
-          >
-            <span>Quay lại</span>
-          </button>
-          <button className={cn("button", styles.button)}>
-            <span>Rời lớp</span>
-            <Icon name="arrow-right" size="24" />
-          </button>
-        </div>
-      </>
-    );
-  };
+            const loadStudents = [];
+            for (const key in responseData.students) {
+                loadStudents.push({
+                    key: key,
+                    name:
+                        responseData.teachers[key].firstName +
+                        " " +
+                        responseData.teachers[key].lastName,
+                    email: responseData.teachers[key].email,
+                    role: "Học sinh",
+                });
+            }
+            setStudents(loadStudents);
 
-  return (
-    <>
-      <Card
-        className={styles.card}
-        title="Thông tin lớp học"
-        classTitle={cn("title-purple", styles.title)}
-        classCardHead={cn(styles.head, { [styles.hidden]: visible })}
-        head={
-          <>
-            <Form
-              className={styles.form}
-              value={search}
-              setValue={setSearch}
-              onSubmit={() => handleSubmit()}
-              placeholder="Tìm theo tên hoặc theo email"
-              type="text"
-              name="search"
-              icon="search"
-            />
-            {/**Button dùng để lấy url lớp học */}
-            {user.roleId === 1 ? (
-              <button
-                className={cn("button-small", styles.button)}
-                onClick={handleGetUrl}
-              >
-                Lấy URL lớp học
-              </button>
-            ) : null}
-            {/* <div className={styles.nav}>
+            setIsLoading(false);
+        };
+
+        fetchData();
+    }, [token]);
+
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [search, setSearch] = useState("");
+    const [visible, setVisible] = useState(false);
+
+    const handleSubmit = (e) => {
+        alert();
+    };
+    const handleInviteMember = () => {
+        return successToast("Đã gửi lời mời!", 2000);
+    };
+    const [openModal, setOpenModal] = useState(false);
+    const [email, setEmail] = useState(true);
+    const [content, setContent] = useState(null);
+    const [urlClass, setUrlClass] = useState("http://example.com");
+    // Xử lí việc rời khỏi lớp học
+    const handleOutClass = () => {};
+
+    // Modal xử lí việc lấy url lớp học
+    const handleGetUrl = () => {
+        setOpenModal(true);
+        setContent(
+            <>
+                <div className={cn("title-green", styles.modaltitle)}>
+                    Lấy URL lớp học
+                </div>
+                <div className={styles.info}>
+                    Đây là đường dẫn đến lớp học của bạn{" "}
+                    <a href={urlClass}>{urlClass}</a>
+                </div>
+                <div className={styles.foot}>
+                    <button
+                        onClick={() => {
+                            setOpenModal(false);
+                        }}
+                        className={cn("button-stroke", styles.button)}
+                    >
+                        <span>Quay lại</span>
+                    </button>
+                    <button
+                        onClick={() => {
+                            navigator.clipboard
+                                .writeText(urlClass)
+                                .then(() => {
+                                    return successToast("Đã sao chép!", 1000);
+                                })
+                                .catch((err) => {
+                                    return errorToast(
+                                        "Sao chép thất bại!",
+                                        1000
+                                    );
+                                });
+                        }}
+                        className={cn("button", styles.button)}
+                    >
+                        <span>Copy</span>
+                        <Icon name="arrow-right" size="24" />
+                    </button>
+                </div>
+            </>
+        );
+    };
+
+    // Modal để thêm học sinh
+    const handleAddingStudent = () => {
+        setOpenModal(true);
+        setContent(
+            <>
+                <div className={cn("title-green", styles.modaltitle)}>
+                    Thêm thành viên
+                </div>
+                <div className={styles.info}>
+                    Thêm các thành viên vào lớp học của bạn.
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "flex-end",
+                        gap: "10px",
+                    }}
+                >
+                    <TextInput
+                        className={styles.field}
+                        label="Địa chỉ email"
+                        name="title"
+                        type="text"
+                        required
+                    />
+                    <button
+                        className={cn("button-stroke", styles.button)}
+                        onClick={handleInviteMember}
+                    >
+                        <span>Thêm</span>
+                    </button>
+                </div>
+                <div className={styles.foot}>
+                    <button
+                        className={cn("button-stroke", styles.button)}
+                        onClick={() => {
+                            setOpenModal(false);
+                        }}
+                    >
+                        <span>Quay lại</span>
+                    </button>
+                </div>
+            </>
+        );
+    };
+    // Modal để show xác nhận rời khỏi lớp
+    const handleOutGroup = () => {
+        setOpenModal(true);
+        setContent(
+            <>
+                <div className={cn("title-green", styles.modaltitle)}>
+                    Xác nhận rời lớp
+                </div>
+                <div className={styles.info}>
+                    Bạn thật sự muốn rời khỏi lớp học này chứ?
+                </div>
+                <div className={styles.foot}>
+                    <button
+                        onClick={() => {
+                            setOpenModal(false);
+                        }}
+                        className={cn("button-stroke", styles.button)}
+                    >
+                        <span>Quay lại</span>
+                    </button>
+                    <button className={cn("button", styles.button)}>
+                        <span>Rời lớp</span>
+                        <Icon name="arrow-right" size="24" />
+                    </button>
+                </div>
+            </>
+        );
+    };
+
+    return (
+        <>
+            <Card
+                className={styles.card}
+                title="Thông tin lớp học"
+                classTitle={cn("title-purple", styles.title)}
+                classCardHead={cn(styles.head, { [styles.hidden]: visible })}
+                head={
+                    <>
+                        <Form
+                            className={styles.form}
+                            value={search}
+                            setValue={setSearch}
+                            onSubmit={() => handleSubmit()}
+                            placeholder="Tìm theo tên hoặc theo email"
+                            type="text"
+                            name="search"
+                            icon="search"
+                        />
+                        {/**Button dùng để lấy url lớp học */}
+                        {user.roleId === 1 ? (
+                            <button
+                                className={cn("button-small", styles.button)}
+                                onClick={handleGetUrl}
+                            >
+                                Lấy URL lớp học
+                            </button>
+                        ) : null}
+                        {/* <div className={styles.nav}>
                             {navigation.map((x, index) => (
                                 <button
                                     className={cn(styles.link, {
@@ -193,41 +257,54 @@ const CustomerList = () => {
                                 </button>
                             ))}
                         </div> */}
-            {/* <Filters
+                        {/* <Filters
                             className={styles.filters}
                             title="Showing 10 of 24 customer"
                         >
                             <Settings />
                         </Filters> */}
-          </>
-        }
-      >
-        <div className={cn(styles.row, { [styles.flex]: visible })}>
-          <Table
-            className={styles.table}
-            activeTable={visible}
-            setActiveTable={setVisible}
-          />
-          <Details
-            className={styles.details}
-            onClose={() => setVisible(false)}
-          />
-        </div>
-      </Card>
-      <Panel
-        roleId={user.roleId}
-        addStudent={handleAddingStudent}
-        outGroup={handleOutGroup}
-      />
-      <Modal
-        outerClassName={styles.outer}
-        visible={openModal}
-        onClose={() => setOpenModal(false)}
-      >
-        {content}
-      </Modal>
-    </>
-  );
+                    </>
+                }
+            >
+                {isLoading && <LoadingSpinner />}
+                {!isLoading && (students || teachers) && (
+                    <div className={cn(styles.row, { [styles.flex]: visible })}>
+                        <Table
+                            className={styles.table}
+                            activeTable={visible}
+                            setActiveTable={setVisible}
+                            teachers={teachers}
+                            students={students}
+                            onActive={handleActive}
+                        />
+
+                        <Details
+                            className={styles.details}
+                            onClose={() => setVisible(false)}
+                            activeUser={activeUser}
+                        />
+                    </div>
+                )}
+                {!isLoading && !teachers && !students && (
+                    <div style={{ textAlign: "center" }}>
+                        Không tìm thấy thành viên nào trong lớp
+                    </div>
+                )}
+            </Card>
+            <Panel
+                roleId={user.roleId}
+                addStudent={handleAddingStudent}
+                outGroup={handleOutGroup}
+            />
+            <Modal
+                outerClassName={styles.outer}
+                visible={openModal}
+                onClose={() => setOpenModal(false)}
+            >
+                {content}
+            </Modal>
+        </>
+    );
 };
 
 export default CustomerList;
