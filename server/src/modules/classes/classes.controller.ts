5e9a71367f802801d79ec4910710792ceff6520e
ptnminh <minhphan113@gmail.com>
2023-12-10 21:29:22 +0700

    fix: auth service

--- server/src/modules/classes/classes.controller.ts
+++ server/src/modules/classes/classes.controller.ts
@@ -186,6 +186,13 @@ export class ClassesController {
       })
     }
     const roleId = user.roleId;
+    const exUser = await this.classesService.findStudentOrTeacherInClass(decoded.classId, user?.id, roleId);
+    if(exUser) {
+      throw new BadRequestException({
+        status: false,
+        message: "Bạn đã tham gia lớp học này"
+      })
+    }
     if (roleId === ROLES.STUDENT) {
       await this.classesService.inviteStudentToClass(decoded.classId, user?.id);
     } else {
