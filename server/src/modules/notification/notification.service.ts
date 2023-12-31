607680099fc5796fe60ae382c0bcc576996dd5ca
ptnminh <minhphan113@gmail.com>
2023-12-31 19:50:29 +0700

    fix: too many connect firebase

--- server/src/modules/notification/notification.service.ts
+++ server/src/modules/notification/notification.service.ts
@@ -3,22 +3,27 @@ import * as firebase from 'firebase-admin';
 import * as config from './firebase.config.json';
 @Injectable()
 export class NotificationService {
+  private __firebase: any;
   constructor() {
-    firebase.initializeApp({
-      credential: firebase.credential.cert({
-        projectId: config.project_id,
-        clientEmail: config.client_email,
-        privateKey: config.private_key,
-      }),
-      databaseURL: 'https://webfinalproject-ef3ea-default-rtdb.firebaseio.com',
-    });
+    this.__firebase =
+      firebase.apps.length === 0
+        ? firebase.initializeApp({
+            credential: firebase.credential.cert({
+              projectId: config.project_id,
+              clientEmail: config.client_email,
+              privateKey: config.private_key,
+            }),
+            databaseURL:
+              'https://webfinalproject-ef3ea-default-rtdb.firebaseio.com',
+          })
+        : firebase.apps[0];
   }
   async createNotification() {
     return 'This action adds a new notification';
   }
 
   public async readNotiLengthFromDB(userId: string) {
-    return firebase
+    return this.__firebase
       .database()
       .ref('notifications')
       .child(userId)
@@ -40,7 +45,7 @@ export class NotificationService {
     currentNotiLength: number;
     newData: any;
   }) {
-    const ref = firebase.database().ref('notifications');
+    const ref = this.__firebase.database().ref('notifications');
     if (!currentNotiLength) {
       return ref.child(userId).set({ '0': newData });
     }
