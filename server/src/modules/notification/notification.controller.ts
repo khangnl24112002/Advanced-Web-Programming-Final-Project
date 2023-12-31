f3294ed8f8e9e7acb965d4141d6661dc83dd00a9
ptnminh <minhphan113@gmail.com>
2023-12-31 17:56:32 +0700

    feat: init firebase service

--- server/src/modules/notification/notification.controller.ts
+++ server/src/modules/notification/notification.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { CurrentUser } from 'src/decorators/users.decorator';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/guards/jwt.auth.guard';
 
 @Controller('notification')
+@ApiTags('Notification')
+// @UseGuards(JwtAuthGuard)
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
   @Post()
-  async createNotification() {
-    return await this.notificationService.createNotification();
+  async createNotification(@CurrentUser('id') userId: string) {
+    const payload = {
+      content: 'This is a notification',
+      createdAt: new Date().toISOString(),
+      isRead: false,
+      title: "You've got a new notification",
+      type: 'notification',
+    };
+    const notiLength =
+      await this.notificationService.readNotiLengthFromDB(userId);
+    const id = notiLength ? notiLength + 1 : 0;
+    const saveNewNotiToUser = await this.notificationService.saveNewNotiToUser({
+      userId,
+      currentNotiLength: notiLength || 0,
+      newData: {
+        ...payload,
+        id,
+        sendingTime: new Date().toISOString(),
+      },
+    });
+    return saveNewNotiToUser;
   }
 }
--- server/src/modules/notification/notification.service.ts
+++ server/src/modules/notification/notification.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import * as firebase from 'firebase-admin';
+import config from './firebase.config.json';
 
 @Injectable()
 export class NotificationService {
+  constructor() {
+    firebase.initializeApp({
+      credential: firebase.credential.cert({
+        projectId: config.project_id,
+        clientEmail: config.client_email,
+        privateKey: config.private_key,
+      }),
+      databaseURL:
+        'https://clinus-1d1d1-default-rtdb.asia-southeast1.firebasedatabase.app',
+    });
+  }
   async createNotification() {
     return 'This action adds a new notification';
+  }
+
+  public async readNotiLengthFromDB(userId: string) {
+    return firebase
+      .database()
+      .ref('notifications')
+      .child(userId)
+      .once('value')
+      .then((snapshot) => {
+        return snapshot.numChildren();
+      })
+      .catch((e) => {
+        console.log('error from read', e);
+      });
+  }
+
+  async saveNewNotiToUser({
+    userId,
+    currentNotiLength,
+    newData,
+  }: {
+    userId: string;
+    currentNotiLength: number;
+    newData: any;
+  }) {
+    const ref = firebase.database().ref('notifications');
+    if (!currentNotiLength) {
+      return ref.child(userId).set({ '1': newData });
+    }
+    const newKeyRef = ref
+      .child(userId)
+      .child((currentNotiLength + 1).toString())
+      .set(newData);
+    return newKeyRef;
   }
 }
