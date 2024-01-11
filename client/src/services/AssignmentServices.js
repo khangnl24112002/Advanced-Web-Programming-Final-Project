00bba496a8f506838ffad7daac4d6623c75a3448
Michael Thinh <michaelthinh1008@gmail.com>
2024-01-11 10:54:09 +0700

    Add: Student submit and create review

--- client/src/services/AssignmentServices.js
+++ client/src/services/AssignmentServices.js
@@ -1,86 +1,115 @@
 import { axiosInstance } from "../utils/axios";
 
 export const assignmentServices = {
-  // API get assignment list
-  getAssignmentList: async (classId) => {
-    try {
-      const response = await axiosInstance.get(`/assignments/${classId}/all`);
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-  getAssignmentReviews: async (assignmentId) => {
-    try {
-      const response = await axiosInstance.get(
-        `/assignments/${assignmentId}/requested-grade-view`
-      );
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-  getAssignmentById: async (assignmentId) => {
-    try {
-      const response = await axiosInstance.get(`/assignments/${assignmentId}`);
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-  // API create new assignment for student
-  createAssignment: async (assignmentData) => {
-    try {
-      const response = await axiosInstance.post("/assignments", assignmentData);
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-  updateAssignment: async (assignmentData, assignmentId) => {
-    try {
-      const response = await axiosInstance.put(
-        `/assignments/${assignmentId}`,
-        assignmentData
-      );
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-  deleteAssignment: async (assignmentId) => {
-    try {
-      const response = await axiosInstance.delete(
-        `/assignments/${assignmentId}`
-      );
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-  uploadFile: async (fileUrl) => {
-    try {
-      const file = new FormData();
-      file.append("file", fileUrl);
-      const response = await axiosInstance.post("/files/upload", file, {
-        headers: {
-          "content-type": "multipart/form-data",
-        },
-      });
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-  markScoreForStudent: async (assignmentId, data) => {
-    try {
-      const response = await axiosInstance.post(
-        `/assignments/${assignmentId}/mark-score`,
-        data
-      );
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
+    // API get assignment list
+    getAssignmentList: async (classId) => {
+        try {
+            const response = await axiosInstance.get(
+                `/assignments/${classId}/all`
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    getAssignmentReviews: async (assignmentId) => {
+        try {
+            const response = await axiosInstance.get(
+                `/assignments/${assignmentId}/requested-grade-view`
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    postAssignmentReview: async (assignmentId, body) => {
+        try {
+            const response = await axiosInstance.post(
+                `/assignments/${assignmentId}/requested-grade-view`,
+                body
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    getAssignmentById: async (assignmentId) => {
+        try {
+            const response = await axiosInstance.get(
+                `/assignments/${assignmentId}`
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    // API create new assignment for student
+    createAssignment: async (assignmentData) => {
+        try {
+            const response = await axiosInstance.post(
+                "/assignments",
+                assignmentData
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    updateAssignment: async (assignmentData, assignmentId) => {
+        try {
+            const response = await axiosInstance.put(
+                `/assignments/${assignmentId}`,
+                assignmentData
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    deleteAssignment: async (assignmentId) => {
+        try {
+            const response = await axiosInstance.delete(
+                `/assignments/${assignmentId}`
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    uploadFile: async (fileUrl) => {
+        try {
+            const file = new FormData();
+            file.append("file", fileUrl);
+            const response = await axiosInstance.post("/files/upload", file, {
+                headers: {
+                    "content-type": "multipart/form-data",
+                },
+            });
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    markScoreForStudent: async (assignmentId, data) => {
+        try {
+            const response = await axiosInstance.post(
+                `/assignments/${assignmentId}/mark-score`,
+                data
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
+    studentSubmitAssignment: async (assignmentId, data) => {
+        try {
+            const response = await axiosInstance.post(
+                `/assignments/${assignmentId}/student-assignment`,
+                data
+            );
+            return response.data;
+        } catch (error) {
+            return error.response.data;
+        }
+    },
 };
