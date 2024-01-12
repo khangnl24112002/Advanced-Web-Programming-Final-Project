62660b676cf5167802bdfcd52e7c3884b6d67ab9
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2024-01-12 08:45:08 +0700

    Fix: Error in noti

--- client/src/pages/Notifications/List/index.js
+++ client/src/pages/Notifications/List/index.js
@@ -13,41 +13,53 @@ import { database } from "../../../configs/firebase";
 const intervals = ["Recent", "New", "This year"];
 
 const List = ({ className }) => {
-    const [sorting, setSorting] = useState(intervals[0]);
+  const [sorting, setSorting] = useState(intervals[0]);
 
-    const { user } = useAuth();
-    const [notifications, setNotifications] = useState([]);
+  const { user } = useAuth();
+  const [notifications, setNotifications] = useState([]);
 
-    useEffect(() => {
-        const starCountRef = ref(database, "notifications/" + user.id);
-        onValue(starCountRef, (snapshot) => {
-            const data = snapshot.val();
-            data?.sort(
-                (a, b) => new Date(b?.createdAt) - new Date(a?.createdAt)
-            );
-            setNotifications(data);
-        });
-    }, []);
+  useEffect(() => {
+    const starCountRef = ref(database, "notifications/" + user.id);
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data !== null) {
+        data?.sort((a, b) => new Date(b?.createdAt) - new Date(a?.createdAt));
+      }
+      setNotifications(data);
+    });
+  }, []);
 
-    const handleReadNotification = (id) => {
-        // Write the new post's data simultaneously in the posts list and the user's post list.
-        const updates = {};
-        updates[`/notifications/${user.id}/${id}`] = {
-            ...notifications[id],
-            isRead: true,
-        };
-        return update(ref(database), updates);
+  const handleReadNotification = (id) => {
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    const updates = {};
+    // Find the noti with id
+    let noti;
+    for (let i = 0; i < notifications?.length; i++) {
+      // eslint-disable-next-line eqeqeq
+      if (id == notifications[i].id) {
+        // eslint-disable-next-line no-unused-vars
+        noti = notifications[i];
+        break;
+      }
+    }
+    updates[`/notifications/${user.id}/${id}`] = {
+      ...noti,
+      isRead: true,
     };
+    // console.log(notifications);
+    // console.log(updates);
+    return update(ref(database), updates);
+  };
 
-    return (
-        <Card
-            className={cn(styles.card, className)}
-            title="Danh sách thông báo"
-            classTitle={cn("title-red", styles.title)}
-            classCardHead={styles.head}
-            head={
-                <>
-                    {/* <Dropdown
+  return (
+    <Card
+      className={cn(styles.card, className)}
+      title="Danh sách thông báo"
+      classTitle={cn("title-red", styles.title)}
+      classCardHead={styles.head}
+      head={
+        <>
+          {/* <Dropdown
             className={styles.dropdown}
             classDropdownHead={styles.dropdownHead}
             value={sorting}
@@ -55,37 +67,33 @@ const List = ({ className }) => {
             options={intervals}
             small
           /> */}
-                </>
-            }
-        >
-            <div className={styles.notifications}>
-                <div className={styles.list}>
-                    {notifications &&
-                        notifications?.map((x, index) => (
-                            <Item
-                                className={cn(styles.item, className)}
-                                item={x}
-                                key={index}
-                                handleReadNotification={() =>
-                                    handleReadNotification(x.id)
-                                }
-                            />
-                        ))}
-                    {!notifications && (
-                        <div className={styles.text}>
-                            Bạn không có thông báo nào
-                        </div>
-                    )}
-                </div>
-                {/* <div className={styles.foot}>
+        </>
+      }
+    >
+      <div className={styles.notifications}>
+        <div className={styles.list}>
+          {notifications &&
+            notifications?.map((x, index) => (
+              <Item
+                className={cn(styles.item, className)}
+                item={x}
+                key={index}
+                handleReadNotification={() => handleReadNotification(x.id)}
+              />
+            ))}
+          {!notifications && (
+            <div className={styles.text}>Bạn không có thông báo nào</div>
+          )}
+        </div>
+        {/* <div className={styles.foot}>
           <button className={cn("button-stroke button-small", styles.button)}>
             <Loader className={styles.loader} />
             <span>Load more</span>
           </button>
         </div> */}
-            </div>
-        </Card>
-    );
+      </div>
+    </Card>
+  );
 };
 
 export default List;
